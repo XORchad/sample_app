7006b319059e3cefe50eda5cfd6a9ca2bcb1fb6a
Samuel del Bello <sdelbello@gmail.com>
2012-10-22 17:05:54 +0200

    done with exe chap 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  #return the logo
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   #return a title on a per pages basis
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
